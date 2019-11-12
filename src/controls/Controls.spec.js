209979e37facc27d90118426f05666099ca6f26d
ngaivoronski <ngaivoronski@gmail.com>
2019-11-12 11:25:48 -0500

    mvp testing

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,72 @@
 // Test away!
+
+import React from "react";
+import Controls from "./Controls";
+import { render, fireEvent } from "@testing-library/react";
+
+
+test('Controls render correctly',()=>{
+    expect(render(<Controls/>)).toMatchSnapshot();
+})
+
+test('cannot be open if locked',()=>{
+    const toggleClosed = jest.fn();
+    const { getByText } = render(
+        <Controls locked={true} closed={true}  toggleClosed={toggleClosed}/>
+    );
+    const openGate = getByText(/Open Gate/i);
+    fireEvent.click(openGate);
+    expect(toggleClosed).toHaveBeenCalledTimes(0);
+})
+
+test('cannot be closed if locked',()=>{
+    const toggleClosed = jest.fn();
+    const { getByText } = render(
+        <Controls locked={true} closed={false}  toggleClosed={toggleClosed}/>
+    );
+    const closeGate = getByText(/Close Gate/i);
+    fireEvent.click(closeGate);
+    expect(toggleClosed).toHaveBeenCalledTimes(0);
+})
+
+test('provide buttons to toggle the closed and locked states.', () => {
+    const { getByTestId } = render(<Controls />);
+    getByTestId(/lock-button/i);
+    getByTestId(/close-button/i);
+});
+
+test("closed button's text changes to reflect if it's closed", () => {
+    const { getByTestId } = render(<Controls closed={true} />);
+    const closeButton = getByTestId(/close-button/i);
+    expect(closeButton.textContent).toBe('Open Gate');
+});
+
+test("closed button's text changes to reflect if it's open", () => {
+    const { getByTestId } = render(<Controls closed={false} />);
+    const closeButton = getByTestId(/close-button/i);
+    expect(closeButton.textContent).toBe('Close Gate');
+});
+
+test("lock button's text changes to reflect if it's locked", () => {
+    const { getByTestId } = render(<Controls locked={true} />);
+    const lockButton = getByTestId(/lock-button/i);
+    expect(lockButton.textContent).toBe('Unlock Gate');
+});
+
+test("lock button's text changes to reflect if it's unlocked", () => {
+    const { getByTestId } = render(<Controls locked={false} />);
+    const lockButton = getByTestId(/lock-button/i);
+    expect(lockButton.textContent).toBe('Lock Gate');
+});
+
+test("the closed toggle button is disabled if the gate is locked", () => {
+    const { getByTestId } = render(<Controls locked={true} />);
+    const closeButton = getByTestId(/close-button/i);
+    expect(closeButton.disabled).toBe(true);
+});
+
+test("the locked toggle button is disabled if the gate is open", () => {
+    const { getByTestId } = render(<Controls closed={false} />);
+    const lockButton = getByTestId(/lock-button/i);
+    expect(lockButton.disabled).toBe(true);
+});
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,66 @@
 // Test away!
+
+import React from "react";
+import Display from "./Display";
+import { render } from "@testing-library/react";
+
+
+test('Display renders correctly',()=>{
+    expect(render(<Display/>)).toMatchSnapshot();
+})
+
+test('defaults to unlocked and open',()=>{
+    const { getByTestId } = render(<Display/>);
+    const unlocked = getByTestId(/locked-div/i);
+    const open = getByTestId(/closed-div/i);
+    expect(unlocked.textContent).toBe('Unlocked');
+    expect(open.textContent).toBe('Open');
+})
+
+test("displays 'Closed' if the closed prop is true",()=>{
+    const { getByTestId } = render(<Display closed={true}/>);
+    const gate = getByTestId(/closed-div/i);
+    expect(gate.textContent).toBe('Closed');
+})
+
+test("displays 'Open' if the closed prop is not true",()=>{
+    const { getByTestId } = render(<Display closed={false}/>);
+    const gate = getByTestId(/closed-div/i);
+    expect(gate.textContent).toBe('Open');
+})
+
+test("displays 'Locked' if the locked prop is true",()=>{
+    const { getByTestId } = render(<Display locked={true}/>);
+    const gate = getByTestId(/locked-div/i);
+    expect(gate.textContent).toBe('Locked');
+})
+
+test("displays 'Unlocked' if the locked prop is not true",()=>{
+    const { getByTestId } = render(<Display locked={false}/>);
+    const gate = getByTestId(/locked-div/i);
+    expect(gate.textContent).toBe('Unlocked');
+})
+
+test("when locked use the red-led class",()=>{
+    const { getByTestId } = render(<Display locked={true}/>);
+    const gate = getByTestId(/locked-div/i);
+    expect(gate.className).toBe('led red-led');
+})
+
+test("when unlocked use the green-led class",()=>{
+    const { getByTestId } = render(<Display locked={false}/>);
+    const gate = getByTestId(/locked-div/i);
+    expect(gate.className).toBe('led green-led');
+})
+
+test("when closed use the red-led class",()=>{
+    const { getByTestId } = render(<Display closed={true}/>);
+    const gate = getByTestId(/closed-div/i);
+    expect(gate.className).toBe('led red-led');
+})
+
+test("when open use the green-led class",()=>{
+    const { getByTestId } = render(<Display closed={false}/>);
+    const gate = getByTestId(/closed-div/i);
+    expect(gate.className).toBe('led green-led');
+})
